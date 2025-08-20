bfc993ed2f391968a60be35a04bb99ce630d5bf5
Prachi9506 <prachimansoorpur@gmail.com>
2025-08-20 17:04:40 +0530

    Add 1878A

---
+++ 1878A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,a;
+	    cin>>n>>a;
+	    vector<int>v(n);
+	    int flag=0;
+	    for(int i=0;i<n;i++){
+	        cin>>v[i];
+	        if(v[i]==a){
+	            flag=1;
+	        }
+	    }
+	    if(flag==1){
+	        cout<<"YES"<<"\n";
+	    }
+	    else{
+	        cout<<"NO"<<"\n";
+	    }
+	}
+ 
+}
