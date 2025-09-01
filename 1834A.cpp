3ce4f4c746910311e1541d4cad731aa2faa73dce
Piyush Goel <piyush.goel2704@gmail.com>
2025-09-01 22:47:35 +0530

    Unit Array

---
+++ 1834A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        int cnt = 0, cnt1 = 0;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            if (x == 1)
+                cnt1++;
+            else
+                cnt++;
+        }
+        int res = 0;
+        while (cnt1 < cnt || cnt % 2 != 0)
+        {
+            res++;
+            cnt--;
+            cnt1++;
+        }
+        cout << res << endl;
+    }
+}
